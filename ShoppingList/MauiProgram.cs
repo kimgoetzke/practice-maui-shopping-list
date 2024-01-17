cbd88ea03c3181f4ae198debf791443d3c7e95ff
Kim Goetzke <kbgoetzke@gmail.com>
2024-01-17 05:45:41 +0000

    Add CommunityToolkit.Maui to MauiProgram

--- ShoppingList/MauiProgram.cs
+++ ShoppingList/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using ShoppingList.Data;
 using ShoppingList.ViewModel;
+using CommunityToolkit.Maui;
 
 namespace ShoppingList;
 
@@ -9,30 +10,21 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
-        
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        }).UseMauiCommunityToolkit();
         builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
-
         builder.Services.AddSingleton<MainPage>();
         builder.Services.AddSingleton<MainViewModel>();
-
         builder.Services.AddTransient<DetailPage>();
         builder.Services.AddTransient<DetailViewModel>();
-        
         builder.Services.AddSingleton<ItemDatabase>();
-        
         builder.Services.AddLogging();
-
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
-
         return builder.Build();
     }
 }
