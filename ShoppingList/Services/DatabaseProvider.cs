0ed58b277cd358860d4f008c1ad6d35b6a79f925
Kim Goetzke <kbgoetzke@gmail.com>
2024-02-08 06:28:12 +0000

    Remove Constants class

--- ShoppingList/Services/DatabaseProvider.cs
+++ ShoppingList/Services/DatabaseProvider.cs
@@ -6,6 +6,14 @@ namespace ShoppingList.Services;
 
 public class DatabaseProvider : IDatabaseProvider
 {
+    private const SQLiteOpenFlags Flags =
+        SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache;
+
+    private const string DatabaseFilename = "ShoppingList.db3";
+
+    private static string DatabasePath =>
+        Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+
     private SQLiteAsyncConnection? _connection;
 
     public async Task<SQLiteAsyncConnection> GetConnection()
@@ -19,8 +27,8 @@ public class DatabaseProvider : IDatabaseProvider
         if (_connection is not null)
             return;
 
-        _connection = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
-        Logger.Log($"Connected to: {Constants.DatabasePath}");
+        _connection = new SQLiteAsyncConnection(DatabasePath, Flags);
+        Logger.Log($"Connected to: {DatabasePath}");
         var itemTable = InitialiseItemTable(_connection);
         var storeTable = InitialiseStoreTable(_connection);
         await Task.WhenAll(itemTable, storeTable);
--- ShoppingList/Utilities/Logger.cs
+++ ShoppingList/Utilities/Logger.cs
@@ -2,10 +2,12 @@ namespace ShoppingList.Utilities;
 
 public static class Logger
 {
+    private const string LoggerTag = "ShoppingList";
+
     public static void Log(string message)
     {
 #if __ANDROID__
-        Android.Util.Log.Info(Constants.LoggerTag, $"[XXX] {message}");
+        Android.Util.Log.Info(LoggerTag, $"[XXX] {message}");
 #elif DEBUG
         Console.WriteLine($"[XXX] {message}");
 #endif
